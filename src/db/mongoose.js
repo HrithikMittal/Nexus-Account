7e5fb95bd32e7cc236ac43f6ea68f26603bba11f
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-06-03 19:50:42 +0530

    removed remote db url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose")
 
-const remoteDB = 'mongodb+srv://adhikansh-atlas:IlShowBgpiOl71JV@cluster0-cy6v9.mongodb.net/nexusAccount?retryWrites=true'
 const localDB = 'mongodb://localhost:27017/accountApp'
 mongoose.connect(remoteDB, {
 	useNewUrlParser: true,
